323fb0d27bcf5736e67ed90fc1200b7077651c1d
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-11-14 23:34:13 +0530

    File Downloader

---
+++ File Downloader/script.py
@@ -0,0 +1,15 @@
+ 
+import requests
+
+url = 'https://assets.tryhackme.com/img/THMlogo.png'
+r = requests.get(url, allow_redirects=True)
+open('THMlogo.png', 'wb').write(r.content)
+
+# -----------------------------------------------------
+ 
+import requests
+
+url = 'https://download.sysinternals.com/files/PSTools.zip'
+r = requests.get(url, allow_redirects=True)
+open('PSTools.zip', 'wb').write(r.content)  
+
